8cb259a217a77ccb417fc1a3f98d86ee04d79fda
Ilya <iosdsbb@gmail.com>
2025-11-07 19:44:55 +0300

    feat(clients/telegram): added doRequest to telegram.go

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,6 +1,12 @@
 package telegram
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"tg-bot-adviser-read/lib/err-utils"
+)
 
 type Client struct {
 	host     string
@@ -15,6 +21,41 @@ func New(host string, token string) Client {
 		client:   http.Client{},
 	}
 }
+
 func newBasePath(token string) string {
 	return "bot" + token
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	const errMsg = "can't do request"
+
+	defer func() {
+		err = err_utils.WrapIfErr(errMsg, err)
+	}()
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
