afffddeb2dc344a5ad67e3c17d54f1ebbb734b7d
Ilya <iosdsbb@gmail.com>
2025-11-08 11:56:48 +0300

    feat(storage): added PickRandom, decodePage to files Storage

--- storage/files/files.go
+++ storage/files/files.go
@@ -2,8 +2,11 @@ package files
 
 import (
 	"encoding/gob"
+	"errors"
+	"math/rand"
 	"os"
 	"path/filepath"
+	"time"
 
 	err_utils "tg-bot-adviser-read/lib/err-utils"
 	"tg-bot-adviser-read/storage"
@@ -14,6 +17,8 @@ type Storage struct {
 }
 
 const defaultPerm = 0774
+
+var ErrNoSavedPages = errors.New("no saved page")
 
 func New(basePath string) Storage {
 	return Storage{basePath: basePath}
@@ -46,6 +51,44 @@ func (s Storage) Save(page *storage.Page) (err error) {
 	}
 
 	return nil
+}
+
+func (s Storage) PickRandom(userName string) (page *storage.Page, err error) {
+	defer func() { err = err_utils.WrapIfErr("can't pick random page", err) }()
+
+	path := filepath.Join(s.basePath, userName)
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, ErrNoSavedPages
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(len(files))
+
+	file := files[n]
+
+	return s.decodePage(filepath.Join(path, file.Name()))
+}
+
+func (s Storage) decodePage(filePath string) (*storage.Page, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err_utils.Wrap("can't decode page", err)
+	}
+	defer func() { _ = f.Close() }()
+
+	var p storage.Page
+
+	if err := gob.NewDecoder(f).Decode(&p); err != nil {
+		return nil, err_utils.Wrap("can't decode page", err)
+	}
+
+	return &p, nil
 }
 
 func fileName(p *storage.Page) (string, error) {
