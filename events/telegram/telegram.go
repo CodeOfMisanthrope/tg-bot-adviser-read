deb8ceb6135fda3af4f5289cadd88004092ff8de
Ilya <iosdsbb@gmail.com>
2025-11-17 22:22:14 +0300

    feat(events): updated Processor

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -1,12 +1,19 @@
 package telegram
 
-import "tg-bot-adviser-read/clients/telegram"
+import (
+	"tg-bot-adviser-read/clients/telegram"
+	"tg-bot-adviser-read/storage"
+)
 
 type Processor struct {
-	tg     *telegram.Client
-	offset int
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-func New(client *telegram.Client) {
-
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
 }
