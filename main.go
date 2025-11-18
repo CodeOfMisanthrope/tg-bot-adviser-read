eeb2c79f7813d4fe9f8f7e4f6a1ccc802d361aab
Ilya <iosdsbb@gmail.com>
2025-11-18 14:06:47 +0300

    feat: updated main

--- main.go
+++ main.go
@@ -3,15 +3,31 @@ package tg_bot_adviser_read
 import (
 	"flag"
 	"log"
-	"tg-bot-adviser-read/clients/telegram"
+	tgClient "tg-bot-adviser-read/clients/telegram"
+	event_consumer "tg-bot-adviser-read/consumer/event-consumer"
+	telegram "tg-bot-adviser-read/events/telegram"
+	"tg-bot-adviser-read/storage/files"
 )
 
 const (
-	tgBotHost = "api.telegram.org"
+	tgBotHost   = "api.telegram.org"
+	storagePath = "storage"
+	batchSize   = 100
 )
 
 func main() {
-	tgClient := telegram.New(mustToken())
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		files.New(storagePath),
+	)
+
+	log.Print("Service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("Service is stopped", err)
+	}
 }
 
 func mustToken() string {
