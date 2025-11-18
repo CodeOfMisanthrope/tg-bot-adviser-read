f1969151b4567048838efbc4cd95b18b7fe81ecd
Ilya <iosdsbb@gmail.com>
2025-11-18 13:43:52 +0300

    feat(consumer/event): added consumer struct

---
+++ consumer/event-consumer/event-consumer.go
@@ -0,0 +1,17 @@
+package event_consumer
+
+import "tg-bot-adviser-read/events"
+
+type Consumer struct {
+	fetcher   events.Fetcher
+	processor events.Processor
+	batchSize int
+}
+
+func New(fetcher events.Fetcher, processor events.Processor, batchSize int) Consumer {
+	return Consumer{
+		fetcher:   fetcher,
+		processor: processor,
+		batchSize: batchSize,
+	}
+}
