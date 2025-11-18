855cd3b9af45d47a4e49bf031e5f492ab16210e5
Ilya <iosdsbb@gmail.com>
2025-11-18 09:54:00 +0300

    feat(events): added fetchText, FetchType

--- events/telegram/telegram.go
+++ events/telegram/telegram.go
@@ -2,6 +2,7 @@ package telegram
 
 import (
 	"tg-bot-adviser-read/clients/telegram"
+	"tg-bot-adviser-read/events"
 	"tg-bot-adviser-read/storage"
 )
 
@@ -16,4 +17,20 @@ func New(client *telegram.Client, storage storage.Storage) *Processor {
 		tg:      client,
 		storage: storage,
 	}
+}
+
+func fetchText(upd telegram.Update) string {
+	if upd.Message == nil {
+		return ""
+	}
+
+	return upd.Message.Text
+}
+
+func fetchType(upd telegram.Update) events.Type {
+	if upd.Message == nil {
+		return events.Unknown
+	}
+
+	return events.Message
 }
